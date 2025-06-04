7de0797b52a9726461bbbd8b4f87a9fe8beeb16d
Dominik Bláha <dominikblaha@Dominiks-MacBook-Air.local>
2025-06-04 16:49:39 +0200

    Another styles

--- src/screens/Game.tsx
+++ src/screens/Game.tsx
@@ -52,13 +52,21 @@ export const Game = () => {
 
   return (
     <div className="h-screen flex flex-col bg-gray-200 touch-none overflow-hidden">
-      <div className="h-12 text-2xl font-bold p-4 flex justify-between items-center">
-        <div className="flex flex-col">
-          <span className="text-gray-600">Score: {score}</span>
-          <span className="text-sm text-gray-400">Best Score: {bestScore}</span>
+      <div className="h-12 text-2xl font-bold px-4 flex justify-between items-center">
+        <div className="flex flex-col flex-1">
+          <span className="text-gray-600">{score}</span>
+          <span className="text-sm text-gray-400">{bestScore}</span>
         </div>
-        <span className="text-4xl">{sum}</span>
-        <span className="text-gray-600">Lives: {lives}</span>
+        <span className="text-4xl flex-1 flex justify-center text-red-950">
+          {sum}
+        </span>
+        <span className="text-gray-600 flex-1 flex justify-end">
+          {Array.from({ length: lives }, (_, i) => (
+            <span key={i} className="text-purple-500 mx-0.5">
+              ♥
+            </span>
+          ))}
+        </span>
       </div>
       <div
         ref={containerRef}
